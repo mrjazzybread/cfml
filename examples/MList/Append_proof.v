bd0080a0a7411ea823ec5852785506230e96c13b
Tiago Soares <tl.soares@campus.fct.unl.pt>
2026-04-20 14:16:25 +0200

    wow...

--- examples/MList/Append_proof.v
+++ examples/MList/Append_proof.v
@@ -6,25 +6,25 @@ From CFML Require Import WPLib.
 From CFML Require Import Stdlib.
 From TLC Require Import LibListZ.
 
-Fixpoint MList A `{EA:Enc A} (L:list A) (p:loc) : hprop :=
-  \exists v, p ~~> v \* match L with
+Fixpoint MList A (L:list A) (p:loc) : hprop :=
+  \exists (v : contents_ A), p ~~> v \* match L with
   | nil => \[v = Nil]
   | x::L' => \exists p', \[v = Cons x p'] \* (p' ~> MList L')
   end.
 
-Definition MList_contents A `{EA:Enc A} (v:contents_ A) (L:list A) : hprop :=
+Definition MList_contents A (v:contents_ A) (L:list A) : hprop :=
   match L with
   | nil => \[v = Nil]
   | x::L' => \exists p', \[v = Cons x p'] \* (p' ~> MList L')
   end.
 
-Lemma MList_contents_iff : forall A (EA:Enc A) v (L:list A),
+Lemma MList_contents_iff : forall A  v (L:list A),
   (MList_contents v L) ==> (MList_contents v L) \* \[v = Nil <-> L = nil].
 Proof using.
   intros. unfold MList_contents. destruct L; xsimpl; auto_false.
 Qed.
 
-Lemma MList_contents_cons : forall A (EA :Enc A) x t (L: list A),
+Lemma MList_contents_cons : forall A x t (L: list A),
  (MList_contents (Cons x t) L) ==>
    \exists xs, \[L = x :: xs] \* t ~> MList xs.
 Proof using.
@@ -33,13 +33,13 @@ Proof using.
   Unshelve. apply (@nil A).
 Qed.
 
-Lemma MList_cons : forall A (p : loc) p' (EA : Enc A) (L : list A) x,
+Lemma MList_cons : forall A (p : loc) p' (L : list A) x,
   p ~~> Cons x p' \* (p' ~> MList L) ==> p ~> MList (x :: L).
 Proof.
   intros. xunfold MList. xsimpl; intros; eauto.
 Qed.
 
-Lemma MList_eq : forall (p:loc) A (EA:Enc A) (L:list A),
+Lemma MList_eq : forall (p:loc) A (L:list A),
   p ~> MList L = (\exists (v:contents_ A), p ~~> v \* MList_contents v L).
 Proof using. intros. destruct L; auto. Qed.
 
@@ -54,15 +54,13 @@ Proof using.
   xcf. xchange (MList_eq p1) ;=> v1.
   xapp. xmatch.
   + xchange MList_eq ;=> v2. xapp. xapp.
-    xchange (MList_contents_iff E Nil) ;=> [H1 H2]. intros.
+    xchange (@MList_contents_iff A) ;=> [H1 H2].
     rewrite H1. 2: auto.
-    rew_list. xchanges <- MList_eq p1. simpl.
+    rew_list. xchanges <- MList_eq p1.
     apply haffine_hpure.
-  + xchanges (MList_contents_cons).
-    intros L1' H1.
-    subst.
+  + xchanges MList_contents_cons.
+    intros L1' H1. subst.
     xapp. 1: constructor.
-    rew_list.
-    xchange MList_cons.
+    rew_list. xchange MList_cons.
     xsimpl.
 Qed.
